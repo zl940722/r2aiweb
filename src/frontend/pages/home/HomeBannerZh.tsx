2f020553b130510629cdbf771bbd8cc7140bdab8
congcong <congcong.dai@r2.ai>
2019-11-28 17:51:42 +0800

    fix home banner

--- src/frontend/pages/home/HomeBannerZh.tsx
+++ src/frontend/pages/home/HomeBannerZh.tsx
@@ -1,70 +1,83 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
-import { makeStyles, createStyles } from "@material-ui/styles";
+// import { makeStyles, createStyles } from "@material-ui/styles";
 import _ from "lodash";
-import Swiper from "swiper";
-import "swiper/css/swiper.css";
+// import Swiper from "swiper";
+// import "swiper/css/swiper.css";
 import { Carousel } from "antd";
 import './banner.css'
 
-const useStyles = makeStyles(() =>
-  createStyles({
-    main: {
-      "& .swiper-pagination-bullet-active": {
-        width: "40px",
-        borderRadius: "5px",
-        background: "#D3323E"
-      }
-    }
-  })
-);
+// const useStyles = makeStyles(() =>
+//   createStyles({
+//     main: {
+//       "& .swiper-pagination-bullet-active": {
+//         width: "40px",
+//         borderRadius: "5px",
+//         background: "#D3323E"
+//       }
+//     }
+//   })
+// );
 
 const HomeBannerZh = (res: any) => {
-  const classes = useStyles();
+  // const classes = useStyles();
   const img = _.get(res, "content.0.zh_img", []);
 
-  const [result,upResult] = useState(<img src={img[0].url} width='100%' alt=""/>);
-  const [isSwiper,upIsSwiper] = useState(false);
+  // const [result, upResult] = useState(<img src={img[0].url} width='100%' alt="" />);
+  // const [isSwiper, upIsSwiper] = useState(false);
 
-  useEffect(()=>{
-    if(!!(window as any).ActiveXObject || "ActiveXObject" in window){//IE
-      upResult(<Carousel>
-        {
-          img.map((itm,index) => <div key={index}>
-            <img width='100%' src={itm.url} alt=""/>
-          </div>)
-        }
-      </Carousel>)
-    }else{
-      upResult(<div className={"swiper-container " + classes.main}>
-        <div className="swiper-wrapper">
-          {
-            img.map((itm,index) => <div key={index} className="swiper-slide">
-              <img width='100%' src={itm.url} alt=""/>
-            </div>)
-          }
-        </div>
-        <div className="swiper-pagination"/>
-      </div>);
-      upIsSwiper(true);
+
+  return <Carousel adaptiveHeight autoplay autoplaySpeed={5000} draggable={false} >
+    {
+      img.map((itm, index) => <div key={index}>
+        <img width='100%' src={itm.url} alt="" />
+      </div>)
     }
+  </Carousel>
 
-  },[]);
+  // useEffect(() => {
+  //   var isOpera = navigator.userAgent.indexOf("Opera") > -1; //判断是否Opera浏览器
+  //   var isIE = navigator.userAgent.indexOf("compatible") > -1 && navigator.userAgent.indexOf("MSIE") > -1 && !isOpera; //判断是否IE浏览器
+  //   var isEdge = navigator.userAgent.toLowerCase().indexOf("edge") > -1 && !isIE; //判断是否IE的Edge浏览器
+  //   var isIE11 = (navigator.userAgent.toLowerCase().indexOf("trident") > -1 && navigator.userAgent.indexOf("rv") > -1);
+  //   if (isIE || isEdge || isIE11) {//IE
+  //     upResult(<Carousel>
+  //       {
+  //         img.map((itm, index) => <div key={index}>
+  //           <img width='100%' src={itm.url} alt="" />
+  //         </div>)
+  //       }
+  //     </Carousel>)
+  //   } else {
+  //     upResult(<div className={"swiper-container " + classes.main}>
+  //       <div className="swiper-wrapper">
+  //         {
+  //           img.map((itm, index) => <div key={index} className="swiper-slide">
+  //             <img width='100%' src={itm.url} alt="" />
+  //           </div>)
+  //         }
+  //       </div>
+  //       <div className="swiper-pagination" />
+  //     </div>);
+  //     upIsSwiper(true);
+  //   }
 
-  useEffect(()=>{
-    if(isSwiper){
-      new Swiper(".swiper-container", {
-        autoplay: true,
-        loop: true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true
-        }
-      });
-    }
-  },[isSwiper]);
+  // }, []);
 
-  return result;
+  // useEffect(() => {
+  //   if (isSwiper) {
+  //     new Swiper(".swiper-container", {
+  //       autoplay: true,
+  //       loop: true,
+  //       pagination: {
+  //         el: ".swiper-pagination",
+  //         clickable: true
+  //       }
+  //     });
+  //   }
+  // }, [isSwiper]);
+
+  // return result;
 
 };
 
