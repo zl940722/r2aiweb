59bcc6b69da024cc426f3a972fced5b745fbc401
congcong <congcong.dai@r2.ai>
2019-12-05 17:58:23 +0800

    fix

--- pages/loginSuccess.tsx
+++ pages/loginSuccess.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const Index: any = () => {
   return <div style={{ paddingTop: 200 }}>
     <div style={{ background: "#F5F5F5", textAlign: "center", margin: "0 300px 380px", padding: 200 }}>
-      <div style={{ display: "flex", justifyContent: "center", paddingTop: 70 }}>
+      <div style={{ display: "flex", justifyContent: "center" }}>
         <img style={{ width: 32, height: 32, marginRight: 20 }} src={"/static/images/price/succ.png"} /><h1
           style={{ color: "#333333" }}>登录成功！</h1>
       </div>
