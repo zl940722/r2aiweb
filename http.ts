77dbe386368af2f86293fba2a26946b9b12473fc
congcong <congcong.dai@r2.ai>
2020-05-07 16:56:49 +0800

    fix url

--- http.ts
+++ http.ts
@@ -1,4 +1,4 @@
-let http = process.env.STRAPI_SERVICE || "http://localhost:1338";
+let http = process.env.STRAPI_SERVICE || process.env.STRAPI_URL || "http://localhost:1338";
 
 if(typeof window === 'object'){
   http='/strapi'
